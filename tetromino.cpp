2708b8b61d10341ceb91cf80686606a87bea1516
Jaroslaw Oska <jaroslaw.oska@gmail.com>
2021-02-14 17:25:53 +0100

    Remove evil code

--- tetromino.cpp
+++ tetromino.cpp
@@ -75,28 +75,14 @@ void Tetromino::rotate()
 
 int Tetromino::rotate(int px, int py)
 {
-    int pi;
-    switch (m_rotation % 4)
-    {
-    case 0: // 0 degrees                // 0  1  2  3
-        pi = py * 4 + px;               // 4  5  6  7
-        break;                          // 8  9 10 11
-                                        //12 13 14 15
 
-    case 1: // 90 degrees               //12  8  4  0
-        pi = 12 + py - (px * 4);        //13  9  5  1
-        break;                          //14 10  6  2
-                                        //15 11  7  3
+    std::function<int(int,int)> r[4]  = {[](int px, int py){return py * 4 + px;},
+                 [](int px, int py){return 12 + py - (px * 4);},
+                 [](int px, int py){return 15 - (py * 4) - px;},
+                 [](int px, int py){return 3 - py + (px * 4);}};
 
-    case 2: // 180 degrees              //15 14 13 12
-        pi = 15 - (py * 4) - px;        //11 10  9  8
-        break;                          // 7  6  5  4
-                                        // 3  2  1  0
-    case 3: // 270 degrees              // 3  7 11 15
-        pi = 3 - py + (px * 4);         // 2  6 10 14
-        break;                          // 1  5  9 13
-    }
-    return pi;
+    return r[m_rotation % 4](px, py);
+
 }
 
 QColor Tetromino::getPos(QColor val, int posX, int posY)
